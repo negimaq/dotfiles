64ca70a15874c4aea93ad278aa6d206393301030
minekaede <nfsy.nico@gmail.com>
2019-01-13 20:43:25 +0900

    Fix vim setup files

--- setup/vim_setup.bat
+++ setup/vim_setup.bat
@@ -4,12 +4,17 @@ rem KaoriYa版Windows用Vimシンボリックリンク作成バッチファイル
 rem 管理者権限チェック・昇格
 openfiles > nul
 if errorlevel 1 (
-    PowerShell.exe -Command Start-Process \"%~f0\" -Verb runas
-    exit
+	PowerShell.exe -Command Start-Process \"%~f0\" -Verb runas
+	exit
 )
+
+rem 設定用パスの取得
+pushd %~dp0..
+set DOTPATH=%CD%
+popd
 
 rem シンボリックリンク作成
 cd %HOMEPATH%
-mklink /D .vim "%~dp0vimfiles"
-mklink _vimrc "%~dp0vimfilesﾂ･vimrc"
+mklink /D .vim "%DOTPATH%\vimfiles"
+mklink _vimrc "%DOTPATH%\vimfiles\vimrc"
 mklink /D vimfiles .vim
--- setup/vim_setup.sh
+++ setup/vim_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-ln -s $SCRIPT_DIR/vimfiles ~/.vim
-ln -s $SCRIPT_DIR/vimfiles/vimrc ~/.vimrc
+# 設定用パスの取得
+SCRIPT_DIR=$(cd $(dirname $0); cd ..; pwd)
+
+# シンボリックリンク作成
+ln -sf $SCRIPT_DIR/vimfiles ~/.vim
+ln -sf $SCRIPT_DIR/vimfiles/vimrc ~/.vimrc
