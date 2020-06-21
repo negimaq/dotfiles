aa2e9bc697c0b5464f591ac4b11d7245e1b50f8e
minekaede <minek4ede@gmail.com>
2020-06-21 20:46:39 +0900

    Modify compet template

--- vimfiles/template/cpp/base-competition.cpp
+++ vimfiles/template/cpp/base-competition.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-#define ar array
+typedef long long ll;
+typedef long double ld;
+#define PB push_back
+#define ALL(v) (v).begin(),(v).end()
+#define FOR(i, n) for(ll (i)=0; (i)<(n); (i)++)
+#define RFOR(i, n) for(ll (i)=(n)-1; (i)>=0; (i)--)
+#define REP(i, x, n) for(ll (i)=(x); (i)<(n); (i)++)
+#define RREP(i, x, n) for(ll (i)=(n)-1; (i)>=x; (i)--)
 
 int main() {
 	ios::sync_with_stdio(0);
