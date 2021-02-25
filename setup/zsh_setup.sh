182200e0ebd5d14ef8e051188addecf66fe9a5c3
negimaq <negimaq@gmail.com>
2021-02-26 02:46:30 +0900

    Add goenv settings

--- setup/zsh_setup.sh
+++ setup/zsh_setup.sh
@@ -15,6 +15,14 @@ ln -sf $DOT_DIR/zshfiles/zshenv ~/.zshenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
+# Golangのインストール先を作成
+if [ ! -e "$HOME/.local/go/bin" ]; then
+	mkdir -p $HOME/.local/go/bin
+fi
+
+# goenvのインストール
+git clone https://github.com/syndbg/goenv.git ~/.goenv
+
 # tmux1.9以上がインストールされている場合にTPMをインストール
 if type "tmux" > /dev/null 2>&1 && [[ ! "$(tmux -V | tr -cd 0123456789.)" < "1.9" ]] ; then
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
