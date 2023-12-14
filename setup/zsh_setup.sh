59d5082fe80700ae072910c8e78d8163b6799d25
negimaq <negimaq@gmail.com>
2023-12-14 22:26:00 +0900

    fix: modify setup conditions

--- setup/zsh_setup.sh
+++ setup/zsh_setup.sh
@@ -12,25 +12,31 @@ ln -sf $DOT_DIR/zshfiles/zshrc ~/.zshrc
 ln -sf $DOT_DIR/zshfiles/zshenv ~/.zshenv
 ln -sf $DOT_DIR/tmux.conf ~/.tmux.conf
 
-# anyenvのインストール
-git clone https://github.com/anyenv/anyenv ~/.anyenv
-~/.anyenv/bin/anyenv install --init
+if [ ! -d ~/.anyenv ]; then
+	# anyenvのインストール
+	git clone https://github.com/anyenv/anyenv ~/.anyenv
+	~/.anyenv/bin/anyenv install --init
 
-# anyenvのプラグインをインストール
-ANYENV_ROOT=$(~/.anyenv/bin/anyenv root)
-mkdir -p $ANYENV_ROOT/plugins
-git clone https://github.com/znz/anyenv-update.git $ANYENV_ROOT/plugins/anyenv-update
-git clone https://github.com/znz/anyenv-git.git $ANYENV_ROOT/plugins/anyenv-git
+	# anyenvのプラグインをインストール
+	ANYENV_ROOT=$(~/.anyenv/bin/anyenv root)
+	mkdir -p $ANYENV_ROOT/plugins
+	git clone https://github.com/znz/anyenv-update.git $ANYENV_ROOT/plugins/anyenv-update
+	git clone https://github.com/znz/anyenv-git.git $ANYENV_ROOT/plugins/anyenv-git
+fi
 
 # asdfのインストール
-ASDF_ROOT=$(~/.asdf)
-git clone https://github.com/asdf-vm/asdf.git $ASDF_ROOT
+if [ ! -d ~/.asdf ]; then
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+fi
 
 # rustupのインストール
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+if ! type "rustup" > /dev/null 2>&1; then
+	curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+fi
 
 # tmux1.9以上がインストールされている場合にTPMをインストール
-if type "tmux" > /dev/null 2>&1 && [[ ! "$(tmux -V | tr -cd 0123456789.)" < "1.9" ]] ; then
+if type "tmux" > /dev/null 2>&1 && \
+	[[ ! "$(tmux -V | tr -cd 0123456789.)" < "1.9" ]] && \
+	[ ! -d ~/.tmux/plugins/tpm ]; then
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	ln -sf $DOT_DIR/tmux.conf ~/.tmux.conf
 fi
